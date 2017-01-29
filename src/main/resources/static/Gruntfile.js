7bf6575801569bd557b8f37d49dcf1a4e44e936b
Ryan Mills <ranemills@gmail.com>
2017-01-29 12:22:07 +0000

    Concat JS and CSS files and exclude bower_components and node_modules from the war

--- src/main/resources/static/Gruntfile.js
+++ src/main/resources/static/Gruntfile.js
@@ -4,8 +4,30 @@ module.exports = function (grunt) {
 
   require('load-grunt-tasks')(grunt);
   grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.initConfig({
+    concat: {
+      js: {
+        src: [
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/tether/dist/js/tether.min.js',
+          'bower_components/bootstrap/dist/js/bootstrap.min.js',
+          'bower_components/angular/angular.js',
+          'bower_components/lodash/dist/lodash.js',
+          'bower_components/moment/min/moment.min.js',
+          'bower_components/angular-moment/angular-moment.min.js'
+        ],
+        dest: 'dist/third-party.js'
+      },
+      css: {
+        src: [
+          'bower_components/bootstrap/dist/css/bootstrap.css',
+          'third-party/font-awesome/css/font-awesome.css'
+        ],
+        dest: 'dist/third-party.css'
+      }
+    },
     babel: {
       options: {
         sourceMap: true,
@@ -48,7 +70,7 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('default', ['babel']);
-  grunt.registerTask('serve', ['babel', 'express:mock', 'watch']);
+  grunt.registerTask('default', ['babel', 'concat:js', 'concat:css']);
+  grunt.registerTask('serve', ['default', 'express:mock', 'watch']);
 
 };
